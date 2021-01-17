674e00d3a5f0c777325af0cbea0805e63147c7e7
Casper <73931922+menelku@users.noreply.github.com>
2021-01-17 19:41:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 
 int main()
 {
+    printf("Wybierz co chcesz zrobić");
+    switch (expression)
+    {
+    case 1:
 
+        break;
+
+    case 2:
+
+        break;
+
+    case 3:
+
+        break;
+
+    default:
+        break;
+    }
     return 0;
 }
